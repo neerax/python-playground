9296a5a29838b4c8bdf833cfce766c2548d6cc9d
Niko <nicola.ranaldo@gmail.com>
2025-05-20 02:03:18 +0200

    common

--- rag/streamlit/pages/search.py
+++ rag/streamlit/pages/search.py
@@ -1,16 +1,8 @@
 import streamlit as st
-from rag.app import RagApp
-from dotenv import load_dotenv
-import os
 
+from common import app, wc
 
-@st.cache_resource()
-def get_app():
-    load_dotenv(dotenv_path="../../rag/rag/.env")
-    return RagApp(os.environ["WEAVIATE_URL"], os.environ["WEAVIATE_API_KEY"])
-
-app = get_app()
-wc = app.get_weaviate_client()
 
 result = app.chunks_near_text(st.text_input("Testo da cercare"),st.number_input("k",1),st.number_input("neighbours",0))
+
 result
--- rag/streamlit/pages/show_objects.py
+++ rag/streamlit/pages/show_objects.py
@@ -1,16 +1,5 @@
 import streamlit as st
-from rag.app import RagApp
-from dotenv import load_dotenv
-import os
-
-
-@st.cache_resource()
-def get_app():
-    load_dotenv(dotenv_path="../../rag/rag/.env")
-    return RagApp(os.environ["WEAVIATE_URL"], os.environ["WEAVIATE_API_KEY"])
-
-app = get_app()
-wc = app.get_weaviate_client()
+from common import app, wc
 
 objects = app.get_documents()
 
